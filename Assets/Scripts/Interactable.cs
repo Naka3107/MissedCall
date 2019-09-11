aefe6da8f48c787fbffffeef331efd4a9c80c141
naka3107 <nakasan3107@gmail.com>
2019-09-10 22:07:55 -0500

    El jugador se mueve y se pueden examinar objetos.

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    protected Vector3 posLastFrame;
+    private bool onInit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Hides shadow now that the object is in inspection mode
+        Renderer uiRender = GetComponent<Renderer>();
+        uiRender.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+
+        // Sets material to non light responsive
+        Material[] matArray = uiRender.materials;
+        matArray[1] = null;
+        uiRender.materials = matArray;
+        uiRender.material.shader = Shader.Find("Unlit/Texture");
+
+        // Makes object only a visible shape. Eliminates physical characteristics
+        Rigidbody rb = GetComponent<Rigidbody>();
+        Collider cd = GetComponent<Collider>();
+        Destroy(rb);
+        Destroy(cd);
+
+        // Interaction lock boolean
+        onInit = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Locks interaction until first mouse click has been released
+        if (onInit)
+        {
+            if (Input.GetMouseButtonUp(0))
+            {
+                onInit = false;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        // Moves object according to mouse movement
+        if (Input.GetMouseButtonDown(0))
+        {
+            posLastFrame = Input.mousePosition;
+
+            // Unlocks mouse position
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            var delta = Input.mousePosition - posLastFrame;
+            posLastFrame = Input.mousePosition;
+
+            var axis = Quaternion.AngleAxis(-90.0f, Vector3.forward) * delta;
+            transform.rotation = Quaternion.AngleAxis(delta.magnitude * 0.25f, axis) * transform.rotation;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            // Locks mouse position
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+}
---
+++ Assets/Scripts/RaycastInteraction.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastInteraction : MonoBehaviour
+{
+    public float HitDistance = 1.0f;
+    public Material tempMaterial;
+    public GameObject Canvas;
+
+    private new Renderer renderer;
+    private Material originalMaterial;
+    private GameObject inspectedObject;
+    private GameObject uiObject;
+
+    bool m_IsExamining = false;
+    bool isHit = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        // If hit interactables (Layer 8)
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, HitDistance, 1 << 8))
+        {
+            // Get the materials of the object and highlight it.
+            Renderer currentRenderer = hit.collider.gameObject.GetComponent<Renderer>();
+            Material original = currentRenderer.materials[1];
+            HighlightBorder(currentRenderer, original);
+
+            // If it's a collectible, then allow examinating it.
+            if (hit.collider.gameObject.tag == "Collectible")
+            {
+                Examinate(hit, original);
+            }
+            isHit = true;
+            return;
+        }
+
+        // If you're no longer examining an object, reset the highlight of the object.
+        if (isHit && !m_IsExamining)
+        { 
+            isHit = false;
+            renderer.sharedMaterial = originalMaterial;
+            renderer = null;
+            return;
+        }       
+    }
+
+    void HighlightBorder(Renderer currentRenderer, Material original)
+    {
+        if (renderer == currentRenderer)
+            return;
+
+        if (currentRenderer && currentRenderer != renderer && renderer)
+            renderer.sharedMaterial = originalMaterial;
+
+        if (currentRenderer)
+            renderer = currentRenderer;
+        else
+            return;
+
+        originalMaterial = original;
+        renderer.sharedMaterial = tempMaterial;
+    }
+
+    void Examinate(RaycastHit hit, Material original)
+    {
+        if (!m_IsExamining)
+        {
+            inspectedObject = hit.collider.gameObject;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                m_IsExamining = true;
+                GetComponentInParent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = false;
+
+                // Generate an instance of the inspected object and set its material to the original one
+                uiObject = Instantiate(inspectedObject);
+                uiObject.GetComponent<Renderer>().material = original;
+
+                // Sets object in front of camera and aligns position and rotation
+                uiObject.transform.SetParent(Canvas.transform, true);
+                uiObject.transform.SetPositionAndRotation(transform.position + transform.TransformDirection(Vector3.forward) * 0.4f, transform.rotation);
+                uiObject.transform.localScale -= new Vector3(0.3f, 0.3f, 0.3f);
+
+                uiObject.GetComponent<Interactable>().enabled = true;
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                m_IsExamining = false;
+                GetComponentInParent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = true;
+                Destroy(uiObject);
+            }
+        }
+    }
+}
